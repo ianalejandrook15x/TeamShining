df03aad45efe33d32cd97fafc956cd332f2245ac
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-04 12:07:10 -0300

    🌙🌙🌙

--- handler.js
+++ handler.js
@@ -341,7 +341,7 @@ export async function handler(chatUpdate) {
                         }
                     }
                     if (m.limit)
-                        conn.reply(m.chat, `Utilizaste *${+m.limit}* ⚡`, m, rcanal)
+                        conn.reply(m.chat, `Utilizaste *${+m.limit}* 🌙`, m, rcanal)
                 }
                 break
             }
