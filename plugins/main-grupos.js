11b70f53ddcd204571b98f68a11e8ed8639fef22
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-03 21:25:13 -0300

    🌸

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -4,29 +4,21 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://qu.ax/cXEr.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*Hola!, te invito a formar parte de la comunidad de 
-> SON GOKU / TEAM
+let txt = `🌙 Ｈ Ｏ Ｌ Ａ 🌙
 
->  Enlace Grupo 🌙TeamShining🌙
+1-Gɾυρσ
+Fυƚαɾσ x Gɾυρσʂ
+*✰* ${group}
 
-*👑* ${group}
+*─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ*
 
-*◆━━━━━━━▣✦▣━━━━━━━━◆ *
+➠ Sι ҽʅ ҽɳʅαƈҽ ɳσ ϝυɳƈισɳα ҽɳƚɾҽ αϙυι⬃
 
-> ⚠️ Enlace anulado? entre aquí! 
+2.-Cαɳαʅ :
+Team Shining Moon 🌙
+*✰* ${canal}
 
-Canal :
-*🪐* ${canal}
-*◆━━━━━━━▣✦▣━━━━━━━━◆ *
-> TEAM MoonLight Team
-
-> CANAL 
-https://whatsapp.com/channel/0029Vablj8M05MUnustq3q1S 
-
-> GRUPO https://chat.whatsapp.com/GpbF3JCTEr2CSj3zLJ1IQj 
-
-
-> 🚩 ${textbot}`
+> ${textbot}`
 await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
 }
 handler.help = ['grupos']
