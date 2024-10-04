ec89a0d23599760def171e5da7ba94f4423fdcd1
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-03 23:35:44 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -4,26 +4,28 @@ import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 
 let tags = {
-  'main': 'Info',
-  'serbot': 'Sub Bots',
+  'main': 'INFO',
+  'serbot': 'SUB BOTS',
   'rpg': 'RPG',
-  'rg': 'Registro',
-  'sticker': 'Stickers',
-  'img': 'Imágenes',
-  'group': 'Grupos',
-  'logo': 'Logo',
-  'nable': 'On/Off', 
-  'tools': 'Herramientas',
-  'nsfw': 'Nsfw', 
-  'owner': 'Creador', 
-  'advanced': 'Avanzado', 
+  'rg': 'REGISTRO',
+  'sticker': 'STICKERS',
+  'img': 'IMAGEBES',
+  'group': 'GRUPOS',
+  'logo': 'LOGO',
+  'nable': 'ON/OFF', 
+  'tools': 'HERRAMIENTAS',
+  'nsfw': 'NSFW', 
+  'owner': 'CREADOR', 
+  'advanced': 'AVANZADO', 
   '+18': '+18',
   
 }
 
 const defaultMenu = {
   before: `  
-> SON GOKU
+┏━━━━━━━━ CREADOR
+┃  ✿︎ ιαɳαʅҽʝαɳԃɾσσƙ15x
+┗━━━━━━━━━━━━━━
 ╭──────────────────✎
 ╰─➤INFO
 │KI : %limit
@@ -33,8 +35,8 @@ const defaultMenu = {
 └➤孫ACTIVO: %uptime 
  %readmore
 `.trimStart(),
-  header: '`MENU X %category`\n\n╭── ⋅ ⋅ ──Sԋιɳιɳɠ Mσσɳ── ⋅ ',
-  body: '│ ⋆ ★%cmd*\n',
+  header: '「`MENU X %category`」\n\n╭── ⋅ ⋅ ──Sԋιɳιɳɠ Mσσɳ── ⋅ ',
+  body: '> 🌙 >%cmd*\n',
   footer: '╰── ⋅ ⋅ ──Sԋιɳιɳɠ Mσσɳ── ⋅ \n',
   after: '',
 }
@@ -137,7 +139,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
-let img = await (await fetch(`https://qu.ax/mtM.jpg`)).buffer()
+let img = await (await fetch(`https://qu.ax/ZuBU.mp4`)).buffer()
     
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
