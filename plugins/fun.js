91deb07adc0e9541cd5e68a50d468762a078bcb9
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-10 02:09:54 -0300

    Update fun.js

--- plugins/fun.js
+++ plugins/fun.js
@@ -68,6 +68,6 @@ mentions: m.mentionedJid
 } : {})}         
 }
 handler.help = ['gay2', 'lesbiana', 'pajero', 'pajera', 'puto', 'puta', 'manco', 'manca', 'rata', 'prostituta', 'prostituto'].map((v) => v + " *@user*")
-handler.tags = ['FUN']
+handler.tags = ['fun']
 handler.command = /^gay2|lesbiana|pajero|pajera|puto|puta|manco|manca|rata|prostituta|prostituto/i
 export default handler
